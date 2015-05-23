9bc6b56d8fd9807068c1974e5c4caa3803444458
Van Aarde Krynauw <nanocell@gmail.com>
2015-05-23 18:11:46 +1000

    Added python bindings for S2Cell and S2CellId classes.

--- python-bindings/geometry_s2.cpp
+++ python-bindings/geometry_s2.cpp
@@ -1,7 +1,11 @@
 
 #include <boost/python.hpp>
+
 #include <s1angle.h>
 #include <s2latlng.h>
+#include <s2region.h>
+#include <s2cellid.h>
+#include <s2cell.h>
 
 using namespace boost::python;
 
@@ -17,7 +21,8 @@ BOOST_PYTHON_MODULE(geometry_s2)
     ;
 
     class_<S2LatLng>("S2LatLng")
-    		.def(init<S1Angle, S1Angle>())
+    	.def(init<S1Angle, S1Angle>())
+
         .def("FromDegrees", &S2LatLng::FromDegrees)
         .def("FromRadians", &S2LatLng::FromRadians)
 
@@ -27,5 +32,22 @@ BOOST_PYTHON_MODULE(geometry_s2)
         .def("is_valid", &S2LatLng::is_valid)
         
     ;
+
+    class_<S2CellId>("S2CellId", init<>())
+        .def(init<uint64>())
+
+        .def("id", &S2CellId::id)
+    ;
+
+    class_<S2Cell>("S2Cell", init<S2CellId>())
+        // .def(init<S2Point>())
+        .def(init<S2LatLng>())
+
+        .def("id", &S2Cell::id)
+        
+    ;
+
+
+
 }
 
